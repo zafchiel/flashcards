7eae0a1a49364629c4463deb2dbfdc9769eb551c
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-09 18:45:23 +0100

    cleaner

---
+++ src/lib/components/sideRail.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { AppRail, AppRailAnchor } from "@skeletonlabs/skeleton";
+  import { page } from "$app/stores";
+</script>
+
+<AppRail class="h-screen">
+  <svelte:fragment slot="lead">
+    <AppRailAnchor href="/" selected={$page.url.pathname === "/"}
+      >Home</AppRailAnchor
+    >
+  </svelte:fragment>
+
+  <AppRailAnchor href="/create" selected={$page.url.pathname === "/create"}
+    >Create</AppRailAnchor
+  >
+  <AppRailAnchor href="/signup" selected={$page.url.pathname === "/signup"}
+    >Sign-Up</AppRailAnchor
+  >
+  <AppRailAnchor href="/login" selected={$page.url.pathname === "/login"}
+    >Login</AppRailAnchor
+  >
+
+  <svelte:fragment slot="trail">
+    <AppRailAnchor href="/profile" selected={$page.url.pathname === "/profile"}
+      >Profile</AppRailAnchor
+    >
+  </svelte:fragment>
+</AppRail>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
+  import SideRail from "$lib/components/sideRail.svelte";
   import "../app.postcss";
-  import { AppRail, AppRailAnchor } from "@skeletonlabs/skeleton";
-  import { page } from "$app/stores";
 </script>
 
 <div class="flex">
-  <AppRail class="h-screen">
-    <svelte:fragment slot="lead">
-      <AppRailAnchor href="/" selected={$page.url.pathname === "/"}
-        >Home</AppRailAnchor
-      >
-    </svelte:fragment>
-
-    <AppRailAnchor href="/create" selected={$page.url.pathname === "/create"}
-      >Create</AppRailAnchor
-    >
-    <AppRailAnchor href="/signup" selected={$page.url.pathname === "/signup"}
-      >Sign-Up</AppRailAnchor
-    >
-    <AppRailAnchor href="/login" selected={$page.url.pathname === "/login"}
-      >Login</AppRailAnchor
-    >
-
-    <svelte:fragment slot="trail">
-      <AppRailAnchor
-        href="/profile"
-        selected={$page.url.pathname === "/profile"}>Profile</AppRailAnchor
-      >
-    </svelte:fragment>
-  </AppRail>
+  <SideRail />
 
   <main class="p-10">
     <slot />
