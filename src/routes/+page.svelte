586363571e6c45fd0ac13b2d4ff5aec903764fd7
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-09 18:46:45 +0100

    typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 </script>
 
 <section>
-  <h1 class="h1 font-bold gradient-heading">Fiski HUH</h1>
+  <h1 class="h1 font-bold gradient-heading">Fishki HUH</h1>
   <p>do zrobienia fishi do nauki słówek</p>
 </section>
