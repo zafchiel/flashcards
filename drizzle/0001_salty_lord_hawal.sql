915eff11d4c68b212a2590f749cc6ae57f4c85c9
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-11 14:51:10 +0100

    new db schema

--- drizzle/0001_salty_lord_hawal.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "auth_user" ADD COLUMN "username" varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE "auth_user" ADD CONSTRAINT "auth_user_username_unique" UNIQUE("username");
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, bigint, varchar } from "drizzle-orm/pg-core";
+import { pgTable, bigint, varchar, text, timestamp, boolean, PgChar, primaryKey } from "drizzle-orm/pg-core";
 
 export const user = pgTable("auth_user", {
 	id: varchar("id", {
@@ -41,22 +41,51 @@ export const key = pgTable("user_key", {
 	})
 });
 
-export const note = pgTable('note', {
-    id: varchar('id', {
-        length: 255
-    }).primaryKey(),
-    userId: varchar('user_id', {
-        length: 15
-    }).notNull().references(() => user.id),
-    title: varchar('title', {
-        length: 255
-    }).notNull(),
-    content: varchar('content', {
-        length: 255
-    }).notNull()
+export const decks = pgTable('decks', {
+	id: varchar('id', {length: 255}).primaryKey(),
+	userId: varchar('user_id', {length: 15}).notNull().references(() => user.id),
+	title: varchar('title', {length: 100}).notNull(),
+	description: text('description'),
+	createdAt: timestamp('created_at').defaultNow(),
+	lastUpdate: timestamp('last_update').defaultNow()
 })
 
+export const flashcards = pgTable('flashcards', {
+	id: varchar('id', {length: 255}).primaryKey(),
+	deckId: varchar('deck_id', {length: 255}).notNull().references(() => decks.id),
+	question: text('question').notNull(),
+	answer: text('answer').notNull()
+})
+
+export const deckProgress = pgTable('deck_progress', {
+	id: varchar('id', {length: 255}).primaryKey(),
+	userId: varchar('user_id', {length: 15}).notNull().references(() => user.id),
+	deckId: varchar('deck_id', {length: 255}).notNull().references(() => decks.id),
+	currentFlashCardId: varchar('current_flashcard_id', {length: 255}).notNull().references(() => flashcards.id),
+	completed: boolean('completed')
+})
+
+export const userFlashcardInteraction = pgTable('user_flashcard_interaction', {
+	id: varchar('id', {length: 255}).primaryKey(),
+	userId: varchar('user_id', {length: 15}).notNull().references(() => user.id),
+	flashcardId: varchar('current_flashcard_id', {length: 255}).notNull().references(() => flashcards.id),
+	interactionDate: timestamp('interaction_date').defaultNow(),
+	correct: boolean('correct')
+
+})
+
+export const tags = pgTable('tags', {
+	id: varchar('id', {length: 15}).primaryKey(),
+	tag_name: varchar('tag_name', {length: 50}).notNull()
+})
+
+export const deckTags = pgTable('deck_tags', {
+	deckId: varchar('deck_id', {length: 255}).notNull().references(() => decks.id),
+	tagId: varchar('id', {length: 15}).references(() => tags.id)
+}, (table) => ({
+	pk: primaryKey({columns: [table.deckId, table.tagId]})
+}))
+
 export type User = typeof user.$inferSelect;
-export type Note = typeof note.$inferSelect;
 export type Key = typeof key.$inferSelect;
 export type Session = typeof session.$inferSelect;
