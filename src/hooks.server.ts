c7aa8379b7b375d5b6ae50e02f828cd5945b30d6
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-12 22:03:40 +0100

    cleanup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,14 +2,12 @@ import { auth } from "$lib/server/lucia";
 import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// we can pass `event` because we used the SvelteKit middleware
 	event.locals.auth = auth.handleRequest(event);
-	if(event.url.pathname.startsWith('/profile')) {
 
+	if(event.url.pathname.startsWith('/profile')) {
 		const session = await event.locals.auth.validate();
 		if(!session) throw redirect(302, '/login');
 		event.locals.user = session.user;
-		
 	}
 	return await resolve(event);
 };
