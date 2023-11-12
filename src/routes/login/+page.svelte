c5212c6977b47ac9fea40c4789cb86152dc606bf
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-12 21:16:33 +0100

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
+
+  const focusInput = (e: HTMLInputElement) => {
+    e.focus();
+  };
 </script>
 
-<h1 class="h1 mb-10">Sign in</h1>
+<section class="flex flex-col justify-center items-center h-full">
+  <div class="variant-soft card p-4">
+    <h1 class="h1 card-header mb-4">Sign in</h1>
+    <form method="post" use:enhance>
+      <div class="flex flex-col gap-3 border-t pt-4">
+        <div>
+          <label for="username" class="label">Username</label>
+          <input
+            use:focusInput
+            name="username"
+            id="username"
+            placeholder="username"
+            class="input"
+          />
+        </div>
 
-<form method="post" use:enhance>
-  <div class="flex flex-col gap-3">
-    <div>
-      <label for="username" class="label">Username</label>
-      <input
-        name="username"
-        id="username"
-        placeholder="username"
-        class="input"
-      />
-    </div>
+        <div>
+          <label for="password" class="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="*****"
+            class="input"
+          />
+        </div>
 
-    <div>
-      <label for="password" class="label">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="*****"
-        class="input"
-      />
-    </div>
-
-    <button class="btn variant-filled">Login</button>
+        <button class="btn variant-filled">Login</button>
+        <p class="text-xs">
+          Don't have account? <a href="/signup" class="anchor"
+            >sign-up instead</a
+          >
+        </p>
+      </div>
+    </form>
   </div>
-</form>
+</section>
