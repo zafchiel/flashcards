2160968de229d368588c0a7995b1f942926fd168
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-13 19:07:44 +0100

    Refactor decks page to display deck information in a card format

--- src/routes/(protected)/profile/decks/+page.svelte
+++ src/routes/(protected)/profile/decks/+page.svelte
@@ -4,9 +4,22 @@
   export let data: PageData;
 </script>
 
-<h1 class="h1">Decks Page</h1>
-{#each data.result as deck (deck.id)}
-  <div>
-    <p>{deck.title}</p>
-  </div>
-{/each}
+<h1 class="h1 p-4">Decks Page</h1>
+<section class="flex gap-3 flex-wrap">
+  {#each data.result as deck (deck.id)}
+    <a
+      href="/profile/decks/${deck.id}"
+      class="card p-4 variant-filled-primary block card-hover"
+    >
+      <div class="card-header">
+        <h2 class="h2 font-semibold">{deck.title}</h2>
+        <p class="text-sm text-opacity-70 text-surface-100">
+          {new Date(deck.createdAt ?? "").toLocaleString("en-GB")}
+        </p>
+      </div>
+      <div class="">
+        <p>{deck.description}</p>
+      </div>
+    </a>
+  {/each}
+</section>
