e640e3950d12c6e2999c09414eb1403746286fc4
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-13 12:36:50 +0100

    profile page avatar

--- src/routes/(protected)/profile/+page.svelte
+++ src/routes/(protected)/profile/+page.svelte
@@ -5,7 +5,21 @@
 </script>
 
 <h3 class="h3">Welcome</h3>
-<h1 class="h1 font-bold">{data.username}</h1>
-<form method="post" action="?/logout">
-  <button class="btn variant-ghost-primary">Log-out</button>
+<div class="flex items-center">
+  <img
+    src={data.user?.avatar}
+    alt="User profile picture"
+    width="64"
+    height="64"
+  />
+  <div
+    class="font-bold border-t border-r border-b self-stretch rounded-r-sm p-3"
+  >
+    <p class="text-3xl break-words">
+      {data.user?.username}
+    </p>
+  </div>
+</div>
+<form method="post" action="?/logout" use:enhance>
+  <button class="btn variant-filled-secondary">Log-out</button>
 </form>
