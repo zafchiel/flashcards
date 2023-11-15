91a4e7f565aa0d221f3006021c7bd3baf86afc85
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-15 17:26:31 +0100

    Add flip animation to flashcard component

--- src/lib/components/flashcardCard.svelte
+++ src/lib/components/flashcardCard.svelte
@@ -5,21 +5,51 @@
   const dispatch = createEventDispatcher();
 
   export let flashcard: Flashcard;
-
   export let showAnswer: Boolean;
+
+  let flipped = false;
 
   const handleSwitchView = () => {
     dispatch("switchView");
+    flipped = !flipped;
   };
 </script>
 
-<div
-  on:click={handleSwitchView}
-  class="card card-hover flex justify-center items-center p-6 text-xl font-semibold md:text-3xl cursor-pointer min-h-[300px]"
->
-  {#if showAnswer}
-    {flashcard.answer}
-  {:else}
-    {flashcard.question}
-  {/if}
+<div class="container">
+  <div
+    on:click={handleSwitchView}
+    class:flipped
+    class="card card-hover flex justify-center items-center p-6 text-xl font-semibold md:text-3xl cursor-pointer min-h-[300px]"
+  >
+    {#if showAnswer}
+      <div class="front">
+        {flashcard.answer}
+      </div>
+    {:else}
+      <div class="back">
+        {flashcard.question}
+      </div>
+    {/if}
+  </div>
 </div>
+
+<style>
+  .container {
+    perspective: 100vh;
+  }
+  .card {
+    transform: rotateX(0);
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+  }
+  .card.flipped {
+    transform: rotateX(180deg);
+  }
+  /* .back,
+  .front {
+    backface-visibility: hidden;
+  } */
+  .back {
+    transform: rotateX(180deg);
+  }
+</style>
