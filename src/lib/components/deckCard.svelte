d1a6c78d85d027aa9c65338c5f15d6872293c0dd
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-16 18:56:15 +0100

    Add delete and edit buttons to deckCard component

--- src/lib/components/deckCard.svelte
+++ src/lib/components/deckCard.svelte
@@ -1,22 +1,57 @@
 <script lang="ts">
   import type { Deck } from "$lib/server/db/schema";
+  import Trash from "$lib/assets/trash.svelte";
+  import Edit from "$lib/assets/edit.svelte";
+  import { getModalStore, type ModalSettings } from "@skeletonlabs/skeleton";
 
   export let deck: Deck;
+
+  const modalStore = getModalStore();
+
+  const modal: ModalSettings = {
+    type: "confirm",
+    title: "Are you sure",
+    body: `You will delete <span class="text-error-500 font-semibold uppercase">${deck.title}</span> deck, this action cannot be undone`,
+    // TRUE if confirm pressed, FALSE if cancel pressed
+    response: async (r: boolean) => {
+      if (r) {
+        const res = await fetch(`/api/decks?deckId=${deck.id}`, {
+          method: "DELETE",
+        });
+        const response = await res.json();
+        if (response.success) {
+          modalStore.close();
+          // toastStore.trigger(successToast);
+        }
+        // else toastStore.trigger(errorToast);
+      }
+    },
+  };
 </script>
 
 <a
   href="/profile/decks/{deck.id}"
-  class="card p-4 variant-filled-primary block card-hover"
+  class="card p-4 variant-filled-primary flex gap-3 card-hover"
 >
-  <div class="">
+  <div>
     <h2 class="h2 font-semibold">{deck.title}</h2>
     <p class="text-sm opacity-60">
       {deck.createdAt?.toLocaleString("en-GB")}
     </p>
+    {#if deck.description}
+      <div>
+        <p>{deck.description}</p>
+      </div>
+    {/if}
   </div>
-  {#if deck.description}
-    <div>
-      <p>{deck.description}</p>
-    </div>
-  {/if}
+  <div class="flex flex-col gap-2">
+    <button class="btn-icon btn-icon-sm variant-filled"><Edit /></button>
+    <button
+      on:click={(e) => {
+        e.preventDefault();
+        modalStore.trigger(modal);
+      }}
+      class="btn-icon btn-icon-sm variant-filled-error"><Trash /></button
+    >
+  </div>
 </a>
