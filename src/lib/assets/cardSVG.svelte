d5b20bef38f3a3d6c96ec5ee04da446e1e32d57c
zafchiel <bialakropla127.9@o2.pl>
2023-11-19 15:51:51 +0100

    home page animation

---
+++ src/lib/assets/cardSVG.svelte
@@ -0,0 +1,18 @@
+<svg width="100%" height="100%" class="stroke-primary-500 absolute inset-0">
+  <g>
+    <rect pathLength="1" rx="16" height="100%" width="100%" />
+  </g>
+</svg>
+
+<style>
+  rect {
+    fill: transparent;
+    stroke-width: 6px;
+    stroke-dasharray: 1;
+    stroke-dashoffset: 1;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    visibility: hidden;
+    transform-style: preserve-3d;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,41 @@
-<script>
+<script lang="ts">
+  import CardSvg from "$lib/assets/cardSVG.svelte";
+  import { onMount } from "svelte";
+  import { timeline } from "motion";
+
+  onMount(async () => {
+    const draw = (progress: number) => ({
+      strokeDashoffset: 1 - progress,
+      visibility: "visible",
+    });
+
+    timeline([
+      ["rect", draw(1), { duration: 2 }],
+      [
+        "div#fancydiv",
+        { rotateX: 180, animationDuration: 2 },
+        { easing: "ease-in-out", delay: 1 },
+      ],
+      ["#firstSide", { opacity: 0, animationDuration: 0.5 }, { at: "-0.3" }],
+      ["#secondSide", { opacity: 1, rotateX: -180, animationDuration: 0.5 }],
+    ]);
+  });
 </script>
 
-<section class="p-10">
-  <h1 class="h1 font-bold gradient-heading">Flashcards HUH</h1>
-  <p>nic</p>
+<section class="p-10 flex flex-col gap-5 items-stretch">
+  <h1 class="h1 font-bold gradient-heading text-center">Flashcards HUH</h1>
+  <div
+    id="fancydiv"
+    class="p-14 min-h-[200px] md:mx-24 grid relative font-bold"
+  >
+    <CardSvg />
+    <p id="firstSide" class="h3 place-self-center">Learn Easily</p>
+    <p id="secondSide" class="h3 place-self-center opacity-0">Remember More</p>
+  </div>
 </section>
+
+<style>
+  section {
+    perspective: 100vh;
+  }
+</style>
