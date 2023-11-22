1867ce30327c91c98587fb948c015a365ab76a3e
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-22 20:06:48 +0100

    fix tooltip position issue

--- src/lib/actions/tooltip.ts
+++ src/lib/actions/tooltip.ts
@@ -2,17 +2,18 @@ export const tooltip = (element: HTMLElement, params: { content: string }) => {
   const tooltip = document.createElement("div");
   const title = element.getAttribute("title");
   const mouseOver = () => {
-    tooltip.innerText = params.content;
+    tooltip.textContent = params.content;
     element.removeAttribute("title")
 
     tooltip.className =
-      "variant-filled-primary fixed p-2 rounded-md border";
+      "variant-filled-primary text-sm absolute p-2 rounded-md border m-0";
     element.appendChild(tooltip);
   };
 
   const mouseMove = (event: MouseEvent) => {
-    tooltip.style.top = `${event.clientY + 5}px`;
-    tooltip.style.left = `${event.clientX + 5}px`;
+      const rect = element.getBoundingClientRect();
+      tooltip.style.top = `${event.clientY - rect.top}px`;
+      tooltip.style.left = `${event.clientX - rect.left}px`;
   };
 
   const mouseOut = () => {
