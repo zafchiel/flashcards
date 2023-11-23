4a0e1ebe6b1cc76f33916fb0efce3a4db8e230f8
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-23 18:45:20 +0100

    Refactor createTags to convert tag names to lowercase and trim whitespace

--- src/lib/server/actions/createTags.ts
+++ src/lib/server/actions/createTags.ts
@@ -2,7 +2,7 @@ import { dbHttp } from "../db"
 import { deckTags } from "../db/schema"
 
 export const createTags = async (tagsArray: string[], deckId: number) => {
-    const data = tagsArray.map(tag => ({deckId, tagName: tag}));
+    const data = tagsArray.map(tag => ({deckId, tagName: tag.toLowerCase().trim()}));
     const result = await dbHttp.insert(deckTags).values(data);
 
     return result;
