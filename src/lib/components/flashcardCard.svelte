bacf1b2986c5fc5448f996f5ec961ffc20c76718
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-25 19:42:01 +0100

    star icon added as learned indicator

--- src/lib/components/flashcardCard.svelte
+++ src/lib/components/flashcardCard.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { Flashcard } from "$lib/server/db/schema";
-  import { SlideToggle } from "@skeletonlabs/skeleton";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
@@ -10,23 +9,39 @@
   const handleSwitchView = () => {
     showAnswer = !showAnswer;
   };
+
+  const handleLearnedChange = (e: Event) => {
+    flashcard.learned = (e.target as HTMLInputElement)?.checked;
+    dispatch("learnedChange");
+  };
 </script>
 
 <div class="container relative">
   {#if flashcard.learned !== null}
-    <SlideToggle
-      size="sm"
-      name="toggleLearned"
-      bind:checked={flashcard.learned}
-      on:change={() => dispatch("learnedChange")}
-      class="absolute top-3 left-3 z-10"
-    >
-      {#if flashcard.learned}
-        Card Learned
-      {:else}
-        Not Learned
-      {/if}
-    </SlideToggle>
+    <div class="absolute top-3 left-3 z-10">
+      <input
+        type="checkbox"
+        id="starToggle"
+        name="starToggle"
+        class="hidden"
+        on:change={handleLearnedChange}
+      />
+      <label for="starToggle" class="cursor-pointer">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill={flashcard.learned ? "currentColor" : "none"}
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          ><polygon
+            points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+          /></svg
+        >
+      </label>
+    </div>
   {/if}
   <button
     on:click={handleSwitchView}
