db141747b770d32b5639c8da575bde28002e94d5
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-11-29 20:31:44 +0100

    fire callback on drawer close

--- src/lib/components/settingsDrawerContents.svelte
+++ src/lib/components/settingsDrawerContents.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-  import { getDrawerStore } from "@skeletonlabs/skeleton";
+  import { getDrawerStore, SlideToggle } from "@skeletonlabs/skeleton";
 
   const drawerStore = getDrawerStore();
+
+  $: {
+    if ($drawerStore.open === false) {
+      console.log("Drawer closed");
+    }
+  }
 </script>
 
 <div class="p-4">
-  <h3 class="h3 font-semibold">Settings</h3>
-  <div>
-    <button
-      on:click={() => {
-        $drawerStore.meta = {
-          ...$drawerStore.meta,
-          message: "cps",
-        };
-      }}
+  <h3 class="h3 font-semibold mb-2">Settings</h3>
+  <div class="p-1">
+    <p>Should this deck be visible to anybody?</p>
+    <SlideToggle
+      bind:checked={$drawerStore.meta.public}
+      name="switchPublic"
+      active="bg-secondary-500"
     >
-      Click me
-    </button>
+      {#if $drawerStore.meta.public}
+        <p>Public</p>
+      {:else}
+        <p>Private</p>
+      {/if}
+    </SlideToggle>
   </div>
 </div>
