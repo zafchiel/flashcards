4012b67f65cfd1b0a136431cb75a13d63940200a
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-12-04 19:37:06 +0100

    main page styles (broken on login)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,24 +22,45 @@
   });
 </script>
 
-<section class="p-10 flex flex-col gap-5">
-  <h1 class="h1 font-bold gradient-heading text-center">Flashcards HUH</h1>
-  <div
-    id="fancydiv"
-    class="p-14 min-h-[200px] md:mx-24 grid relative font-bold"
-  >
-    <CardSvg />
-    <p id="firstSide" class="h3 place-self-center">Learn Easily</p>
-    <p id="secondSide" class="h3 place-self-center opacity-0">Remember More</p>
-  </div>
-</section>
+<main class="min-h-[100svh]">
+  <section class="p-10 flex flex-col items-center gap-5">
+    <div class="max-w-xl text-center">
+      <h1 class="h1 font-bold gradient-heading text-center">Flashcards HUH</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni
+        distinctio ut nihil eius commodi modi at alias. Fugit saepe modi aperiam
+        nesciunt dicta repudiandae vero, magnam ea eaque animi.
+      </p>
+    </div>
+    <div
+      id="fancydiv"
+      class="p-14 min-h-[200px] md:mx-24 grid relative font-bold"
+    >
+      <CardSvg />
+      <p id="firstSide" class="h3 place-self-center">Learn Easily</p>
+      <p id="secondSide" class="h3 place-self-center opacity-0">
+        Remember More
+      </p>
+    </div>
+  </section>
 
-<div class="flex gap-3 justify-center items-center">
-  <a href="/signup" class="btn variant-filled-primary">Get Started</a>
-  <a href="/login" class="btn variant-outline-secondary">Sign-in</a>
-</div>
+  <div class="flex gap-3 justify-center items-center">
+    <a href="/signup" class="btn variant-filled-primary">Get Started</a>
+    <a href="/login" class="btn variant-outline-secondary">Sign-in</a>
+  </div>
+</main>
 
 <style>
+  main {
+    background: rgb(21, 23, 31);
+    background: linear-gradient(
+      45deg,
+      rgba(21, 23, 31, 1) 0%,
+      rgba(61, 36, 37, 0.7) 81%,
+      rgba(212, 22, 60, 0.4) 100%
+    );
+  }
+
   section {
     perspective: 100vh;
   }
