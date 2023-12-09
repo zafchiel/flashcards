92f9910493bf9f58c1d5817de7caef8e75f2902b
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-12-09 16:02:10 +0100

    Refactor protected path handling in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,21 @@
 import { auth } from "$lib/server/lucia";
 import { redirect, type Handle } from "@sveltejs/kit";
 
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = auth.handleRequest(event);
+const protectedPaths = ["/profile", "/decks"];
 
-	if(event.url.pathname.startsWith('/profile') || event.url.pathname.startsWith('/decks')) {
-		const session = await event.locals.auth.validate();
-		if(!session) throw redirect(302, '/auth?t=signin');
-		event.locals.user = session.user;
-	}
-	return await resolve(event);
+const isPathProtected = (path: string, protectedPathsArray: string[]) => {
+  return protectedPathsArray.some((protectedPath) =>
+    path.startsWith(protectedPath)
+  );
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.auth = auth.handleRequest(event);
+
+  if (isPathProtected(event.url.pathname, protectedPaths)) {
+    const session = await event.locals.auth.validate();
+    if (!session) throw redirect(302, "/auth?t=signin");
+    event.locals.user = session.user;
+  }
+  return await resolve(event);
 };
