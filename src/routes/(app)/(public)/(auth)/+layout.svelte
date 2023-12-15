dc81b9868739f2ec48312688acc1a8dd7a9bd683
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-12-15 21:35:45 +0100

    auth page image

--- src/routes/(app)/(public)/(auth)/+layout.svelte
+++ src/routes/(app)/(public)/(auth)/+layout.svelte
@@ -1,6 +1,19 @@
+<script lang="ts">
+  // import image from "$lib/assets/black-card.jpg";
+</script>
+
 <main class="min-h-[100svh] flex flex-col justify-center items-center">
-  <div class="flex-grow flex justify-center items-center">
-    <slot />
+  <div class="p-4 flex-grow flex justify-center items-center">
+    <div class="hidden md:block p-4 max-w-4xl">
+      <enhanced:img
+        src="../../../../lib/assets/black-card.jpg"
+        alt="Logo"
+        class="rounded-lg shadow-lg"
+      />
+    </div>
+    <div class="min-w-max">
+      <slot />
+    </div>
   </div>
   <p class="text-sm opacity-60 p-1">
     @zafchiel, {new Date().getFullYear()}&#169;
