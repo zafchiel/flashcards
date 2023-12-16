eb005c495496c8ea25aa74c887fa82b51bf00729
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-12-16 15:15:25 +0100

    styling

--- src/routes/(app)/(public)/(auth)/+layout.svelte
+++ src/routes/(app)/(public)/(auth)/+layout.svelte
@@ -2,7 +2,7 @@
   class="min-h-[100svh] flex flex-col justify-center items-center bg-gradient-to-br from-surface-800 from-50% to-secondary-700 to-98%"
 >
   <div class="p-4 w-full flex-grow flex justify-around items-center">
-    <div class="hidden md:block p-4 max-w-4xl">
+    <div class="hidden md:block p-4 max-w-5xl">
       <enhanced:img
         src="../../../../lib/assets/unsplash.jpg"
         alt="Logo"
@@ -14,6 +14,7 @@
     </div>
   </div>
   <p class="text-sm opacity-60 p-1">
-    @zafchiel, {new Date().getFullYear()}&#169;
+    <a href="https://github.com/zafchiel" target="_blank">@zafchiel</a>
+    &#169;{new Date().getFullYear()}
   </p>
 </main>
