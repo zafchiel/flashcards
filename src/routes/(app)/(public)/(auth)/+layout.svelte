04897573bf1eba0f7fda2a653008a548a240d0f6
zafchiel <bialakropla127.9@o2.pl>
2023-12-22 12:45:54 +0100

    chore: cleaner import

--- src/routes/(app)/(public)/(auth)/+layout.svelte
+++ src/routes/(app)/(public)/(auth)/+layout.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+  import imageSrc from "$lib/assets/unsplash.jpg?enhanced";
+</script>
+
 <main
   class="min-h-[100svh] flex flex-col justify-center items-center bg-gradient-to-br from-surface-800 from-50% to-secondary-700 to-98%"
 >
   <div class="p-4 w-full flex-grow flex justify-around items-center">
     <div class="hidden md:block p-4 max-w-5xl">
       <enhanced:img
-        src="../../../../lib/assets/unsplash.jpg"
+        src={imageSrc}
         alt="Logo"
         class="rounded-lg shadow-lg"
       />
