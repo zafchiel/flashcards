fc9c9a1e80b565ef8e83ddbdf8bc38c6d2fe4a0c
zafchiel <bialakropla127.9@o2.pl>
2023-12-26 14:05:21 +0100

    form loading state

--- src/lib/components/searchInput.svelte
+++ src/lib/components/searchInput.svelte
@@ -1,16 +1,39 @@
 <script>
   import SearchIcon from "$lib/assets/searchIcon.svelte";
   import { enhance } from "$app/forms";
+  import LoaderIcon from "$lib/assets/loaderIcon.svelte";
+
+  let isFormLoading = false;
 </script>
 
-<form method="POST" action="?/searchDecks" use:enhance>
-  <div
-    class="input-group input-group-divider grid-cols-[auto_1fr_auto]"
-  >
+<form
+  method="POST"
+  action="?/searchDecks"
+  use:enhance={() => {
+    setTimeout(() => {
+      isFormLoading = true;
+    }, 300);
+
+    return ({ update }) => {
+      isFormLoading = false;
+      update();
+    };
+  }}
+>
+  <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
     <div class="input-group-shim">
       <SearchIcon />
     </div>
     <input type="search" name="search" placeholder="Search..." />
-    <button class="variant-filled-primary">Submit</button>
+    <button class="variant-filled-primary" disabled={isFormLoading}>
+      {#if isFormLoading}
+        <span class="animate-spin">
+          <!-- ⏳ -->
+          <LoaderIcon />
+        </span>
+      {:else}
+        Submit
+      {/if}
+    </button>
   </div>
 </form>
