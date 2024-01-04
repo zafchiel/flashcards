ce880758d7bf16939a45fba866a016fa4dab2a5f
zafchiel <bialakropla127.9@o2.pl>
2024-01-04 14:07:00 +0100

    fonts fallbacks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,32 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import { skeleton } from '@skeletonlabs/tw-plugin'
-import forms from '@tailwindcss/forms'
+import { join } from "path";
+import type { Config } from "tailwindcss";
+import { skeleton } from "@skeletonlabs/tw-plugin";
+import forms from "@tailwindcss/forms";
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}"
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
 
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'crimson',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+    skeleton({
+      themes: {
+        preset: [
+          {
+            name: "crimson",
+            enhancements: true,
+          },
+        ],
+      },
+    }),
+  ],
 } satisfies Config;
