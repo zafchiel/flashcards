fc61d6b198c35287d7b254a355952d2cdb3654c6
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2024-02-01 18:36:00 +0100

    Update image source in auth layout

--- src/routes/(app)/(public)/(auth)/+layout.svelte
+++ src/routes/(app)/(public)/(auth)/+layout.svelte
@@ -1,15 +1,11 @@
-<script lang="ts">
-  import imageSrc from "$lib/assets/unsplash.jpg?enhanced";
-</script>
-
 <main
   class="min-h-[100svh] flex flex-col justify-center items-center bg-gradient-to-br from-surface-800 from-50% to-secondary-700 to-98%"
 >
   <div class="p-4 w-full flex-grow flex justify-around items-center">
     <div class="hidden md:block p-4 max-w-5xl">
       <enhanced:img
-        src={imageSrc}
-        alt="Auth page image"
+        src="../../../../lib/assets/unsplash.jpg"
+        alt="Auth page"
         class="rounded-lg shadow-lg"
       />
     </div>
