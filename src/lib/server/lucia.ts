74d0221fe12cef139ca3d69db34d8f3f547fd41d
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2024-02-04 13:13:40 +0100

    google oauth prod fix

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -5,34 +5,41 @@ import { pg } from "@lucia-auth/adapter-postgresql";
 import { pool } from "./db";
 
 import { github, google } from "@lucia-auth/oauth/providers";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+import {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} from "$env/static/private";
 
 export const auth = lucia({
-	env: dev ? "DEV" : "PROD",
-	middleware: sveltekit(),
-    adapter: pg(pool, {
-        user: "auth_user",
-        key: "user_key",
-        session: "user_session",
-      }),
+  env: dev ? "DEV" : "PROD",
+  middleware: sveltekit(),
+  adapter: pg(pool, {
+    user: "auth_user",
+    key: "user_key",
+    session: "user_session",
+  }),
 
   getUserAttributes: (data) => {
     return {
       username: data.username,
       avatar: data.avatar,
-    }
-  }
+    };
+  },
 });
 
 export const githubAuth = github(auth, {
   clientId: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-})
+});
 
 export const googleAuth = google(auth, {
   clientId: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  redirectUri: "http://localhost:5173/auth/google/callback",
-})
+  redirectUri: dev
+    ? "http://localhost:5173/auth/google/callback"
+    : `${process.env.VERCEL_URL}/auth/google/callback`,
+});
 
 export type Auth = typeof auth;
