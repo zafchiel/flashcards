d131fcb0a5f26ccd32ce30a631e1e65782e400f0
zafchiel <bialakropla@proton.me>
2024-02-25 22:32:36 +0100

    more subtle peview image animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,18 +10,18 @@
       const xVal = e.clientX;
       const yVal = e.clientY;
 
-      const roationX = -8 * ((yVal - height / 2) / height);
-      const rotationY = 8 * ((xVal - width / 2) / width);
+      const roationX = -4 * ((yVal - height / 2) / height);
+      const rotationY = 4 * ((xVal - width / 2) / width);
 
-      const animationString = `rotateX(${roationX}deg) rotateY(${rotationY}deg) scale(1.05)`;
+      const animationString = `rotateX(${roationX}deg) rotateY(${rotationY}deg) scale(1.02)`;
       node.style.transform = animationString;
-      node.style.transition = "0.1s"
+      node.style.transition = "0.1s";
     };
 
     const mouseOutHanlder = () => {
       node.style.transform = "rotateX(0deg) rotateY(0deg)";
       node.style.transition = "0.5s ease-in-out";
-    }
+    };
 
     node.addEventListener("mousemove", mouseOverHandler);
     node.addEventListener("mouseout", mouseOutHanlder);
@@ -33,7 +33,6 @@
       },
     };
   };
-
 </script>
 
 <svelte:head>
@@ -41,55 +40,71 @@
 </svelte:head>
 
 <main>
-<div
-  class="min-h-[100dvh] rounded-b-xl md:p-8 flex flex-col gap-8 items-center justify-center bg-gradient-to-b from-surface-800 from-30% to-primary-600 to-98% shadow-md"
->
-  <section
-    class="p-4 m-2 md:p-10 flex flex-col items-center gap-5 card variant-soft-surface shadow-lg"
+  <div
+    class="min-h-[100dvh] rounded-b-xl md:p-8 flex flex-col gap-8 items-center justify-center bg-gradient-to-b from-surface-800 from-30% to-primary-600 to-98% shadow-md"
   >
-    <div class="max-w-xl mb-5">
-      <h1 class="h1 font-bold gradient-heading">Flashcards\HUH</h1>
-      <p class="text-sm md:text-base">
-        Here you can learn and practice various subjects using interactive
-        flashcards. Start exploring our collection of flashcards and enhance
-        your learning experience today!
-      </p>
-      <div class="p-3 flex flex-wrap gap-3 items-center max-w-xs">
-        <a href="/auth?t=signup" class="btn variant-filled-primary flex-1 font-semibold"
-          >Get Started</a
-        >
-        <a href="/auth?t=signin" class="btn variant-outline-secondary flex-1 font-semibold"
-          >Sign-in</a
-        >
+    <section
+      class="p-4 m-2 md:p-10 flex flex-col items-center gap-5 card variant-soft-surface shadow-lg"
+    >
+      <div class="max-w-xl mb-5">
+        <h1 class="h1 font-bold gradient-heading">Flashcards\HUH</h1>
+        <p class="text-sm md:text-base">
+          Here you can learn and practice various subjects using interactive
+          flashcards. Start exploring our collection of flashcards and enhance
+          your learning experience today!
+        </p>
+        <div class="p-3 flex flex-wrap gap-3 items-center max-w-xs">
+          <a
+            href="/auth?t=signup"
+            class="btn variant-filled-primary flex-1 font-semibold"
+            >Get Started</a
+          >
+          <a
+            href="/auth?t=signin"
+            class="btn variant-outline-secondary flex-1 font-semibold"
+            >Sign-in</a
+          >
+        </div>
       </div>
-    </div>
-    <CardSvg />
-  </section>
-  <section class="max-w-[80vw]">
-    <div use:previewImageAction class="p-2 rounded-xl bg-surface-500/30 backdrop-blur-md shadow-md hover:shadow-xl">
-      <enhanced:img
-        src="../lib/assets/preview.jpeg"
-        alt="Preview"
-        class="rounded-md"
-        sizes="min(1434px, 100vw)"
-      />
-    </div>
-  </section>
-</div>
-<section class="grid md:grid-cols-2 gap-4 max-w-[80vw] mx-auto my-4 md:p-10">
-  <div class="flex flex-col gap-4 items-start">
-    <h2 class="text-xl font-bold">Get ready for exam</h2>
-    <p>Flashcards help you memorise everything you need to know. Make your learning easier and shorter.</p>
-    <a href="/auth?t=signup" class="btn variant-filled-primary font-semibold"
+      <CardSvg />
+    </section>
+    <section class="max-w-[80vw]">
+      <div
+        use:previewImageAction
+        class="p-2 rounded-xl bg-surface-500/30 backdrop-blur-md shadow-md hover:shadow-xl"
+      >
+        <enhanced:img
+          src="../lib/assets/preview.jpeg"
+          alt="Preview"
+          class="rounded-md"
+          sizes="min(1434px, 100vw)"
+        />
+      </div>
+    </section>
+  </div>
+  <section class="grid md:grid-cols-2 gap-4 max-w-[80vw] mx-auto my-4 md:p-10">
+    <div class="flex flex-col gap-4 items-start">
+      <h2 class="text-xl font-bold">Get ready for exam</h2>
+      <p>
+        Flashcards help you memorise everything you need to know. Make your
+        learning easier and shorter.
+      </p>
+      <a href="/auth?t=signup" class="btn variant-filled-primary font-semibold"
         >Get Started</a
       >
-  </div>
-  <enhanced:img src="../lib/assets/cardPreview.jpeg" alt="Cards preview" class="rounded-xl" />
-</section>
+    </div>
+    <enhanced:img
+      src="../lib/assets/cardPreview.jpeg"
+      alt="Cards preview"
+      class="rounded-xl"
+    />
+  </section>
 </main>
 <footer class="bg-primary-600 rounded-t-xl p-3 flex justify-between shadow-md">
   <h3 class="text-lg md:text-xl font-semibold">Flashcards</h3>
-  <p><a href="https://github.com/zafchiel">@zafchiel</a>, {new Date().getFullYear()}</p>
+  <p>
+    <a href="https://github.com/zafchiel">@zafchiel</a>, {new Date().getFullYear()}
+  </p>
 </footer>
 
 <style>
